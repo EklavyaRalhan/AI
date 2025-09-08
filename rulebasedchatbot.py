7bc9c9e90e7bb1c83b853cd7fc58260d5cc6644b
EklavyaRalhan <122105141+EklavyaRalhan@users.noreply.github.com>
2025-09-08 19:01:37 +0400

    Add files via upload

--- rulebasedchatbot.py
+++ rulebasedchatbot.py
@@ -41,4 +41,50 @@ def recommend():
 
     show_help()
 
+def packing_tips():
+    print(Fore.CYAN + "TravelBot: Where to?")
+    location = normalize_input(input(Fore.YELLOW + "You: "))
+    print(Fore.CYAN + "TravelBot: How many days?")
+    days = input(Fore.YELLOW + "You: ")
+
+    print(Fore.GREEN + f"TravelBot: Packing tips for {days} days in  {location}")
+    print(Fore.GREEN + "TravelBot: Pack versatile clothes")
+    print(Fore.GREEN + "TravelBot: Bring chargers/adapters")
+    print(Fore.GREEN + "TravelBot: Check the weather forecast")
+
+def tell_joke():
+    print(Fore.YELLOW + f"TravelBot: {random.choice(jokes)}")
+
+def show_help():
+    print(Fore.MAGENTA + "\nI can: ")
+    print(Fore.GREEN + "Suggest travel spots (type recommendation) ")
+    print(Fore.GREEN + "Offer packing tips (type packing) ")
+    print(Fore.GREEN + "Tell a joke (type joke) ")
+    print(Fore.CYAN + "Type exit or bye to end \n")
+
+def chat():
+    print(Fore.CYAN + "TravelBot: Hello, I am TravelBot, what is your name?")
+    name = input(Fore.YELLOW + "You: ")
+    print(Fore.GREEN + f"Nice to meet you {name}")
+
+    show_help()
+
+    while True:
+        user_input = input(Fore.YELLOW + f"{name}: ")
+        user_input = normalize_input(user_input)
+
+        if "recommend" in user_input or "suggest" in user_input:
+            recommend()
+        elif "pack" in user_input or "packing" in user_input:
+            packing_tips()
+        elif "joke" in user_input or "funny" in user_input:
+            tell_joke()
+        elif "exit" in user_input or "bye" in user_input:
+            break
+        else:
+            print(Fore.RED + "Could you please rephrase?")
+
+if __name__ == "__main__":
+    chat()
+    
 
